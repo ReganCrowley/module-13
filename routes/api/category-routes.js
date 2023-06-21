1d1c6cc98630b6dd281c605b379d02253f1bdbf5
Regan <regancrowley1@gmail.com>
2023-06-21 19:58:32 +1000

    added routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,81 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// localhost:3001/api/categories/:id
+router.get('/:id', async (req, res) => {
+  try {
+    const CategoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const CategoryData = await Category.create(req.body);
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', (req, res) => {
+  Category.update(
+    req.body,
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((updatedcategory) => {
+      // Sends the updated book as a json response
+      res.json(updatedcategory);
+    })
+    .catch((err) => res.json(err));
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const CategoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!CategoryData) {
+      res.status(404).json({ message: 'No Category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(CategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
